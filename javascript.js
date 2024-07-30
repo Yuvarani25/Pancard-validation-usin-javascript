8fc60f9758c89392ca2dcb359a6f306c4a099f59
Yuvarani25 <145268723+Yuvarani25@users.noreply.github.com>
2024-07-30 17:06:19 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function validatePAN() {
+    var pan = document.getElementById("panNumber").value;
+
+    var panPattern = /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/;
+    if (!panPattern.test(pan)) {
+        alert("Invalid PAN number");
+        return false; 
+    }
+    alert("Submitted Successfully")
+    return true;
+}
